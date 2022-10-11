771cd2ec736b8cfe8e3141ced4cc20d9d4c39946
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-10-11 23:37:05 +0300

    show icon based on current season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -14,9 +14,12 @@ const SeasonDisplay = (props) => {
   console.log(season);
   const text =
     season === "winter" ? "Burr,Its chilly!!" : "Let's get to the beach!";
+  const icon = season === "winter" ? "snowflake" : "sun";
   return (
     <div>
+      <i className={`${icon} icon`} />
       <h1>{text}</h1>
+      <i className={`${icon} icon`} />
     </div>
   );
 };
